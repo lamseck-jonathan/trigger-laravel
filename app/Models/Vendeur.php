57a7d7c3837df294e03034964f84a1e208380ea5
Razafiarison Tafita <tafita.raza@outlook.com>
2021-12-29 19:12:26 +0300

    Creating vendeur model

---
+++ app/Models/Vendeur.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Vendeur extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'vd_name',
+        'salaire' 
+    ];
+
+    public function getVdNameAttribute($value) {
+        return ucfirst($value);
+    }
+
+    public function setVdNameAttribute($value) {
+        $this->attributes['vd_name'] = strtolower($value);
+    }
+
+}
